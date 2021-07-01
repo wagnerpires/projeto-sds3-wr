59d7c1d4a60aa79e5f541fd230e399b450b5f222
wagnerpires <wagnerpires@hotmail.com>
2021-07-01 14:50:52 -0300

    Mudanças cosméticas

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -3,6 +3,7 @@ const Footer = () => {
         <footer className="footer mt-auto py-3 bg-dark">
             <div className="container">
                 <p className="text-light">App desenvolvido por <a href="https://github.com/wagnerpires" target="_blank" rel="noreferrer">Wagner Pires</a></p>
+                <p className="text-light"><small><strong>WR - TECNOLOGIA EM SISTEMAS - (61)98162-6852</strong><br /></small></p>
                 <p className="text-light"><small><strong>Semana Spring React</strong><br />
       Evento promovido pela escola DevSuperior: <a href="https://instagram.com/devsuperior.ig" target="_blank" rel="noreferrer">@devsuperior.ig</a></small></p>
             </div>
--- frontend/src/pages/Dashboard/index.tsx
+++ frontend/src/pages/Dashboard/index.tsx
@@ -9,21 +9,21 @@ const Dashboard = () => {
         <>
             <NavBar />
             <div className="container">
-                <h1 className="text-primary py-3">Dashboard de Vendas</h1>
+                <h1 className="text-primary py-3">Painel de Vendas</h1>
 
                 <div className="row px-3">
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Taxa de Sucesso (%)</h5>
+                        <h5 className="text-center text-secondary">Taxa de Sucesso por Vendedor (%)</h5>
                         <BarChart />
                     </div>
                     <div className="col-sm-6">
-                        <h5 className="text-center text-secondary">Todas as Vendas</h5>
+                        <h5 className="text-center text-secondary">Total por Vendedor (%)</h5>
                         <DonutChart />
                     </div>
                 </div>
 
                 <div className="row px-3">
-                    <h2 className="text-primary">Todas as Vendas</h2>
+                    <h2 className="text-primary">Listagem Geral de Vendas</h2>
                 </div>
                 <DataTable />
             </div>
